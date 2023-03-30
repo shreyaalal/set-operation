1acf1893adb7491e26cf0db456197284c0afd831
Shreya <shreyalal2025@gmail.com>
2023-03-30 18:59:40 +0530

    Add files via upload

---
+++ assignment-8.py
@@ -0,0 +1,19 @@
+# Define the sets A and B
+A = {1, 2, 3, 4, 5}
+B = {4, 5, 6, 7, 8}
+
+# Intersection of A and B
+intersection = A.intersection(B)
+print("Intersection of A and B:", intersection)
+
+# Union of A and B
+union = A.union(B)
+print("Union of A and B:", union)
+
+# Difference between A and B
+difference = A.difference(B)
+print("Difference between A and B:", difference)
+
+# Symmetric difference between A and B
+symmetric_difference = A.symmetric_difference(B)
+print("Symmetric difference between A and B:", symmetric_difference)
